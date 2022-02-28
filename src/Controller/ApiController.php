8e1ba5d2dd18b60e3d505217e2d5cd29a1e4c4dc
Juan Antonio González <jgonzalezrivera@eci.safanet.es>
2022-02-28 15:18:25 +0100

    fetching from nvd API

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+
+class ApiController
+{
+
+    private $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client=$client;
+    }
+
+    public function fetchAll()
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://services.nvd.nist.gov/rest/json/cves/1.0/'
+        );
+
+        $content = $response->getContent();
+ 
+        $content = $response->toArray();
+
+        return new JsonResponse($content);
+    }
+}
